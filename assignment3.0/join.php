8b1b97d55171b96b13463ee18b76022c7e417d32
Collin Daniel Turner <cdturner@silk2.uvm.edu>
2015-10-13 18:02:40 -0400

    duncan still sucks

--- assignment3.0/join.php
+++ assignment3.0/join.php
@@ -10,4 +10,5 @@ print '<article>';
 print '<h2>Assignment 2.0</h2>';
 print '</article>';
 include "footer.php";
+
 ?>
--- bin/pass.php
+++ bin/pass.php
@@ -6,5 +6,4 @@ $dbReader = "Ex93ikcA2Arz";
 $dbWriter = "81rRyXUyQdLuG3f7";
 $dbAdmin  = "O4jzCmM0hJQ7ln9o";
 
-
 ?>
